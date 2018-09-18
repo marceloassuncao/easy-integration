aeeef976b4814ecc760451d2bd1a88038d74bf0d
marceloassuncao <marceloassuncao2000@gmail.com>
2018-09-17 23:16:25 -0300

    adicionando o plugin

---
+++ easy_integ/includes/class-easy-integ.php
@@ -0,0 +1,23 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) :
+    exit;
+endif;
+
+if ( ! class_exists( 'easyIntegClass' ) ) :
+
+    class easyIntegClass {
+
+
+    public function __construct()
+    {
+        //ACTIONS...
+    }
+    //FUNCTIONS...
+    
+
+
+}    
+    $easyIntegClass = new easyIntegClass();
+
+endif;
