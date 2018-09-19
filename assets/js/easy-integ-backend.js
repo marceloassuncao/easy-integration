520af502d6411aa2223531619424cedd1dea8de1
angeleded <alanalegredacunha@gmail.com>
2018-09-19 00:44:43 -0300

    :construction: first version launched - 0.0.1

--- assets/js/easy-integ-backend.js
+++ assets/js/easy-integ-backend.js
@@ -1,2 +1,23 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function() {
+  function handleClick(e) {
+    let data = {
+      action: 'esi_async_request',
+    };
+
+    jQuery.ajax({
+      url: ajaxurl,
+      method: 'POST',
+      dataType: 'json',
+      data: data,
+      beforeSend: function() {
+        jQuery('.esi-btn').attr('disabled', true);
+      },
+      success: function(response) {
+        jQuery('.esi-btn').attr('disabled', false);
+        console.log(response);
+      },
+    });
+  }
+
+  jQuery('.esi-btn').on('click', handleClick);
 });
--- includes/class-easy-integ.php
+++ includes/class-easy-integ.php
@@ -1,23 +1,63 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) :
-    exit;
+if (!defined('ABSPATH')):
+    exit();
 endif;
 
-if ( ! class_exists( 'easyIntegClass' ) ) :
-
-    class easyIntegClass {
-
-
-    public function __construct()
+if (!class_exists('easyIntegClass')):
+    class easyIntegClass
     {
-        //ACTIONS...
+        public function __construct()
+        {
+            add_action('admin_menu', array($this, 'esi_init'));
+
+            add_action('wp_ajax_esi_async_request', array(
+                $this,
+                esi_async_request
+            ));
+        }
+
+        public function esi_init()
+        {
+            add_menu_page(
+                'Integração',
+                'Integração',
+                'manage_options',
+                'esi-page',
+                array($this, 'esi_menu_page_callback'),
+                'dashicons-welcome-widgets-menus',
+                90
+            );
+        }
+
+        public function esi_menu_page_callback()
+        {
+            ?>
+                <section class="esi-container">
+                    <button class="esi-btn">Iniciar</button>
+                </section>
+            <?php
+        }
+
+        public function esi_async_request()
+        {
+            if (
+                !isset($_POST['action']) ||
+                $_POST['action'] !== 'esi_async_request'
+            ):
+                echo json_encode(array(
+                    'status' => 'error'
+                ));
+                wp_die();
+            endif;
+
+            do_action('esi_start_integration');
+
+            echo json_encode(array(
+                'status' => 'success'
+            ));
+            wp_die();
+        }
     }
-    //FUNCTIONS...
-    
-
-
-}    
     $easyIntegClass = new easyIntegClass();
-
 endif;
